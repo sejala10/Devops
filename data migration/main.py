4a7114789bf631875b64ed7b048817f83ff9bf43
sejala10 <131762205+sejala10@users.noreply.github.com>
2023-08-22 17:18:44 +0530

    Delete data migration directory

--- data migration/main.py
+++
@@ -1,7 +0,0 @@
-ftpUrl = "demoftp.codeteddy.com"
-userName = "codeteddy"
-password = "my#supersecret#password"
-print("\nDisplaying FTP details\n")
-print("FTP URL: " + ftpUrl)
-print("FTP User Name: " + userName)
-print("Password: " + password)
