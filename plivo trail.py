81274d3316ef0b52defa29d2b70b79e3a40c5ebb
sejala10 <131762205+sejala10@users.noreply.github.com>
2023-08-22 17:23:02 +0530

    Delete plivo trail.py

--- plivo trail.py
+++
@@ -1,9 +0,0 @@
-import plivo
-Auth_Id = 'MAM2E0YWIYMGY5NDY0MJ'
-Auth_Token = 'ZTdmYTFkYjk1MDkxODM4ZDRiNzhhODk0MmUxMWZj'
-client = plivo.RestClient('Auth_Id','Auth_Token')
-response = client.messages.create(
-         src='<6303595458>',
-         dst='<9866250045>',
-         text='python msg',)
-print(response)
